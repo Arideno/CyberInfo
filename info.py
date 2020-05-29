4df91ce621a2613ef8508185d7d0bebb947f73f2
Dasha <grimalskayad@gmail.com>
2020-05-29 21:50:12 +0300

    Sort

--- info.py
+++ info.py
@@ -20,14 +20,27 @@ def team_list():
     js_teams = r.json()
     month = ct - 2592000
     for team in js_teams:
+        if not team["last_match_time"]:
+            continue
         if team["last_match_time"] > month:
             identifier = team["team_id"]
             name = team["name"]
             wins = team["wins"]
             losses = team["losses"]
-            t = Team(identifier, name, wins, losses)
-            teams.append(t)
+            if not wins:
+                wins = 0
+            if not losses:
+                losses = 0
+            if identifier and name:
+                t = Team(identifier, name, wins, losses)
+                teams.append(t)
+
+    teams.sort(key=lambda team: team.name.lower())
     return teams
+
+tl = team_list()
+for team in tl:
+    print(team)
 
 
 
