5204f52ebf9b66f38679bf01dd792608e5aa1355
Dasha <grimalskayad@gmail.com>
2020-05-30 00:34:41 +0300

    Matches by team

--- info.py
+++ info.py
@@ -70,16 +70,62 @@ def recent_matches(number=5):
         if match["duration"]:
             duration = get_duration(int(match["duration"]))
             winner = radiant if match["radiant_win"] else dire
-        if dire and radiant:
-            match = Match(name, radiant, dire, duration, winner)
-            matches.append(match)
+            if dire and radiant:
+                match = Match(name, radiant, dire, duration, winner)
+                matches.append(match)
+    return matches
 
+def team_matches(name, number=5):
+    identifier = id_by_name(name)
+    if identifier is None:
+        return None
+    matches = []
+    r = requests.get(f"https://opendota.com/api/teams/{identifier}")
+    tname = r.json()["name"]
+    r2 = requests.get(f"https://opendota.com/api/teams/{identifier}/matches")
+    js_r2 = r2.json()
+    for i in range(number):
+        match = js_r2[i]
+        mname = match["league_name"]
+        if match["radiant"]:
+            radiant = tname
+            dire = match["opposing_team_name"]
+        else:
+            radiant = match["opposing_team_name"]
+            dire = tname
+        if match["duration"]:
+            duration = get_duration(int(match["duration"]))
+            winner = radiant if match["radiant_win"] else dire
+            match = Match(mname, radiant, dire, duration, winner)
+            matches.append(match)
     return matches
 
 
-m = recent_matches()
-for match in m:
-    print(match)
+def id_by_name(name):
+    r = requests.get("https://opendota.com/api/teams")
+    teams = r.json()
+    for t in teams:
+        if levenshtein_distance(name.lower(), t["name"].lower()) < 4:
+            return t["team_id"]
+    return None
+
+
+def levenshtein_distance(word1, word2):
+    n = len(word1) + 1
+    m = len(word2) + 1
+    matrix = [['*' for _ in range(n)] for _ in range(m)]
+    for i in range(n):
+        matrix[0][i] = i
+    for j in range(m):
+        matrix[j][0] = j
+    for i in range(1, m):
+        for j in range(1, n):
+            if word1[j - 1] != word2[i - 1]:
+                matrix[i][j] = min(matrix[i - 1][j], matrix[i - 1][j - 1], matrix[i][j - 1]) + 1
+            else:
+                matrix[i][j] = matrix[i - 1][j - 1]
+    return matrix[m - 1][n - 1]
+
 
 
 
