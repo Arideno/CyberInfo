cb2a2f8e497bb43462a96071de37796ac0cc09f1
Dasha <grimalskayad@gmail.com>
2020-05-30 16:51:33 +0300

    Player

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player:
     def __init__(self, nick):
         self.nick = nick
         self.name = None
+        self.romanized = None
         self.age = None
         self.country = None
         self.photo = None
@@ -16,8 +17,30 @@ class Player:
     def __repr__(self):
         return f"{self.nick}, {self.name}, {self.age}, {self.country}, {self.teams}"
 
+    def fill_player_info(self):
+        details = dota_obj.get_player_info(self.nick, True)
+        info = details["info"]
+        self.name = info["name"]
+        try:
+            self.romanized = info["romanized_name"]
+        except:
+            pass
+        birth = info["birth_details"]
+        age = ""
+        for i in range(len(birth)):
+            if len(birth)-4 < i < len(birth)-1:
+                age += birth[i]
+        self.age = int(age)
+        self.country = info["country"]
+        self.photo = info["image"]
+        teams = details["history"]
+        for t in teams:
+            self.teams.append(t["name"])
 
-    def fill_[alyer]
 
-player_details = dota_obj.get_player_info('hao', True)
-print(player_details)
+
+# det = dota_obj.get_player_info('kuroky', True)
+# print(det)
+player = Player("kuroky")
+player.fill_player_info()
+print(player)
